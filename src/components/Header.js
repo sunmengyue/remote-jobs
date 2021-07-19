29c37bd76c2f32a2bca83941a142a211738db3fd
bardrabbit <bardrabbit@gmail.com>
2021-07-19 16:23:03 -0400

    add clip path to the banner

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,16 +3,18 @@ import '../css/Header.css';
 
 const Header = () => {
   return (
-    <div className="banner">
-      <h1>Remote Optimal</h1>
-      <p>You decide where to live and work</p>
-      <form>
-        <input
-          type="text"
-          placeholder="Title, companies, expertise, or benefits"
-        />
-        <i className="fas fa-briefcase"></i>
-      </form>
+    <div className="header">
+      <div className="banner">
+        <h1>Remote Optimal</h1>
+        <p>You decide where to live and work</p>
+        <form>
+          <input
+            type="text"
+            placeholder="Title, companies, expertise, or benefits"
+          />
+          <i className="fas fa-briefcase"></i>
+        </form>
+      </div>
     </div>
   );
 };
