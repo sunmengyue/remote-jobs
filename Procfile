c1c009659bd9b5af181ab6c7565b2f0c178609cf
sunmengyue <mengyuesun@gmail.com>
2021-11-08 09:32:47 -0500

    prepare for deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: node src/backend/index.js
---
+++ src/backend/index.js
@@ -0,0 +1,18 @@
+const jsonServer = require('json-server');
+const app = jsonServer.create();
+const path = require('path');
+const express = require('express');
+const middlewares = jsonServer.defaults();
+const router = jsonServer.router('db.json');
+const port = process.env.PORT || 3001;
+
+app.use(middlewares);
+app.use(express.static(path.join(__dirname, '../../build')));
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, '../../build', 'index.html'));
+});
+app.use(router);
+
+app.listen(port, () => {
+  console.log('server is running');
+});
